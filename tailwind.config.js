2f6ccdfc718e6e66d878b0dfd25d505463888875
rlorxl <gooodgirl08@gmail.com>
2023-02-01 22:26:29 +0900

    Docs : Edit tailwind config colors name

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "primary-color1": "#B095FF",
-        "primary-color2": "#62C46E",
-        gray1: "#999999",
-        "fontColor-gray1": "#6F6F6F",
-        "fontColor-gray2": "#C7C7C7",
-        "borderColor-gray1": "#D9D9D9",
+        "primary-violet": "#B095FF",
+        "primary-green": "#62C46E",
+        "basic-gray1": "#999999",
+        "text-gray1": "#6F6F6F",
+        "text-gray2": "#C7C7C7",
+        "border-gray1": "#D9D9D9",
         error: "#FF5252",
       },
     },
