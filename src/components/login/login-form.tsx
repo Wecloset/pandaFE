f911221764ccd0a975d360f36849361b03d24362
JIHOON LEE <94469974+jiji-hoon96@users.noreply.github.com>
2023-02-19 01:57:22 +0900

    Refactor : useform setting props

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
+import { errorLine, errorMessage } from "../../lib/error";
 import createHashedPassword from "../../lib/hash";
 import { regExgPw, regExpEm } from "../../lib/regInput";
 import { axiosPost } from "../../lib/services";
@@ -35,30 +36,26 @@ const LoginForm: NextPage = () => {
       <input
         {...register("email", { required: true, pattern: regExpEm })}
         placeholder="아이디(이메일)"
-        className={`${
-          errors.email && "border-b-error"
-        } mb-2 h-9 border-b-2 bg-transparent px-2 text-white placeholder:text-white`}
+        className={errorLine(errors.email)}
       />
-      {errors?.email?.type === "required" && (
-        <p className="mb-2 px-2 text-error">이메일을 입력해주세요</p>
-      )}
-      {errors?.email?.type === "pattern" && (
-        <p className="mb-2 px-2 text-error">잘못된 이메일 형식입니다.</p>
-      )}
+      {errorMessage(errors?.email?.type, "required", "이메일을 입력해주세요")}
+      {errorMessage(errors?.email?.type, "pattern", "잘못된 이메일 형식입니다")}
       <input
         type="password"
         {...register("password", { required: true, pattern: regExgPw })}
         placeholder="비밀번호"
         autoComplete="false"
-        className={`${
-          errors.password && "border-b-error"
-        } mb-2 h-9  border-b-2 bg-transparent px-2 text-white placeholder:text-white`}
+        className={errorLine(errors.password)}
       />
-      {errors?.password?.type === "required" && (
-        <p className="mb-2 px-2 text-error">비밀번호를 입력해주세요</p>
+      {errorMessage(
+        errors?.password?.type,
+        "required",
+        "비밀번호를 입력해주세요",
       )}
-      {errors?.password?.type === "pattern" && (
-        <p className="mb-2 px-2 text-error">잘못된 비밀번호 형식입니다.</p>
+      {errorMessage(
+        errors?.password?.type,
+        "pattern",
+        "잘못된 비밀번호 형식입니다",
       )}
       <button
         type="submit"
---
+++ src/lib/error.tsx
@@ -0,0 +1,8 @@
+export const errorLine = (error: any) =>
+  `${
+    error && "border-b-error"
+  } && "border-b-error" mb-2  h-9 border-b-2 px-3 text-black placeholder:text-textColor-gray-100`;
+
+export const errorMessage = (error: any, type: string, message: string) => {
+  return error === type && <p className="mb-2 px-2 text-error">{message}</p>;
+};
