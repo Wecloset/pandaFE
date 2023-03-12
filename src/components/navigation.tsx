6b5f67db492b21b638a3f16cdd1b18c27a08493a
JIHOON LEE <94469974+jiji-hoon96@users.noreply.github.com>
2023-03-12 18:57:21 +0900

    feat : mypage click edit
    로그인 확인 여부로 mypage 로 들어가는 조건 생성

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -5,50 +5,71 @@ import { useRouter } from "next/router";
 
 const Navigation: NextPage = () => {
   const { pathname } = useRouter();
-  const { data: sesssion } = useSession();
+  const { data: session } = useSession();
   return (
-    <footer className="fixed bottom-0 z-20 flex h-12 w-[390px] items-center justify-between bg-black p-5 text-lg shadow dark:border-gray-600 dark:bg-gray-800">
-      <Link href="/market">
-        <button
-          className={`${
-            pathname === "/market" ? "text-primary-violet" : "text-white"
-          }`}
-        >
-          MARKET
-        </button>
-      </Link>
-      <Link href="/style">
-        <button
-          className={`${
-            pathname === "/style" ? "text-primary-violet" : "text-white"
-          }`}
-        >
-          STYLE
-        </button>
-      </Link>
-      <Link href="/mypage">
-        <button
-          className={`${
-            pathname === "/mypage" || pathname === "/mypage/profile"
-              ? "text-primary-violet"
-              : "text-white"
-          }`}
-        >
-          MYPAGE
-        </button>
-      </Link>
-      <Link href={sesssion?.user ? "/dm" : "/login"}>
-        <button
-          className={`${
-            pathname === "/login" || pathname === "dm"
-              ? "text-primary-violet"
-              : "text-white"
-          }`}
-        >
-          {sesssion?.user ? "DM" : "LOGIN"}
-        </button>
-      </Link>
-    </footer>
+    <div className="fixed bottom-0 z-20 flex h-12 w-[390px] items-center justify-between bg-black p-5 text-lg shadow dark:border-gray-600 dark:bg-gray-800">
+      <div>
+        <Link href="/market">
+          <button
+            className={`${
+              pathname === "/market" ? "text-primary-violet" : "text-white"
+            }`}
+          >
+            MARKET
+          </button>
+        </Link>
+      </div>
+      <div>
+        <Link href="/style">
+          <button
+            className={`${
+              pathname === "/style" ? "text-primary-violet" : "text-white"
+            }`}
+          >
+            STYLE
+          </button>
+        </Link>
+      </div>
+      <div>
+        {session ? (
+          <Link href="/mypage">
+            <button
+              className={`${
+                pathname === "/mypage" || pathname === "/mypage/profile"
+                  ? "text-primary-violet"
+                  : "text-white"
+              }`}
+            >
+              MYPAGE
+            </button>
+          </Link>
+        ) : (
+          <button
+            className={`${
+              pathname === "/mypage" || pathname === "/mypage/profile"
+                ? "text-primary-violet"
+                : "text-white"
+            }`}
+            onClick={() => alert("로그인 후 이용가능합니다")}
+          >
+            MYPAGE
+          </button>
+        )}
+      </div>
+      <div>
+        <Link href={session?.user ? "/dm" : "/login"}>
+          <button
+            className={`${
+              pathname === "/login" || pathname === "dm"
+                ? "text-primary-violet"
+                : "text-white"
+            }`}
+          >
+            {session?.user ? "DM" : "LOGIN"}
+          </button>
+        </Link>
+      </div>
+    </div>
   );
 };
 
