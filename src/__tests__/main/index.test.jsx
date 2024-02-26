3c8fd07b655638fcfa53d4d9a3263d06a0f831fc
rlorxl <gooodgirl08@gmail.com>
2024-02-26 23:24:34 +0900

    rebuild

--- src/__tests__/main/index.test.jsx
+++ src/__tests__/main/index.test.jsx
@@ -2,6 +2,7 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Keywords from "../../components/main/keywords";
 import Recommend from "../../components/main/recommend";
+import { describe, it } from "node:test";
 
 describe("main", () => {
   it("로그인 되어 있을 때 키워드가 렌더링 되는지 확인", async () => {
