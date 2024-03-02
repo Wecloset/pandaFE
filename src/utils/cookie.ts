09f5652166c96ee2a184ce4547c64064208d4252
rlorxl <gooodgirl08@gmail.com>
2024-03-02 19:46:17 +0900

    rebuild

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -3,19 +3,13 @@ export const setCookie = (
   value: string | boolean,
   time: number,
 ) => {
-  let date = new Date();
+  const date = new Date();
   date.setTime(date.getTime() + time * 60 * 1000);
-  let expires = "; expires=" + date.toUTCString();
+  const expires = "; expires=" + date.toUTCString();
   document.cookie = `${name}=${value + "" || ""}; expires=${expires}`;
 };
 
 export const isCookie = (name: string) => {
-  let matches = document.cookie.match(
-    new RegExp(
-      "(?:^|; )" +
-        name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
-        "=([^;]*)",
-    ),
-  );
-  return matches ? decodeURIComponent(matches[1]) : undefined;
+  const matches = document.cookie.match("(^|;) ?" + name + "=([^;]*)(;|$)");
+  return matches ? decodeURIComponent(matches[2]) : undefined;
 };
